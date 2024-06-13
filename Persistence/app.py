d2db1f69270790bfdf3f503183427e622c27dc18
Your Name <saul.vera787@gmail.com>
2024-06-13 19:15:17 -0400

    correcting import syntax

--- Persistence/app.py
+++ Persistence/app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from .user_controller import user_controller
-from .country_city_controller import country_city_controller
-from .amenity_controller import amenity_controller
-from .place_controller import place_controller
-from .review_controller import review_controller
-from flask_sqlalchemy import SQLAlchemy
-
+from ..Api import user_controller
+from ..Api import country_city_controller
+from ..Api import amenity_controller
+from ..Api import place_controller
+from ..Api import review_controller
 
 app = Flask(__name__)
 app.register_blueprint(user_controller)
 app.register_blueprint(country_city_controller)
 app.register_blueprint(amenity_controller)
 app.register_blueprint(place_controller)
-app.register_blueprint(review_controller.py)
+app.register_blueprint(review_controller)
