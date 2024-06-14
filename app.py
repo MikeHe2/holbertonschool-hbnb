2e6bd590195ef290c304ff0f7902675aea8566e6
MikeHe2 <michael.he30@hotmail.com>
2024-06-14 18:29:22 -0400

    ADD: app.py with message when in route

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
+from Api.user_controller import user_controller
+from Api.country_city_controller import country_city_controller
+from Api.amenity_controller import amenity_controller
+from Api.place_controller import place_controller
+from Api.review_controller import review_controller
+# from Api.flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.register_blueprint(user_controller)
+app.register_blueprint(country_city_controller)
+app.register_blueprint(amenity_controller)
+app.register_blueprint(place_controller)
+app.register_blueprint(review_controller)
+# $PLACEHOLDER$
+
 @app.route('/')
-def welcome():
+def hello():
     return "Welcome to Hbnb part 1"
 
-@app.route('/hello')
-def hello():
-    return "Hello, World!"
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
