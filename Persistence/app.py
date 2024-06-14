35b0d6ed094bbb68b4801ee6ce17ee98254e9ac9
Your Name <saul.vera787@gmail.com>
2024-06-13 21:07:58 -0400

    adding tun statement

--- Persistence/app.py
+++ Persistence/app.py
@@ -8,8 +8,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
 app.register_blueprint(user_controller)
 app.register_blueprint(country_city_controller)
 app.register_blueprint(amenity_controller)
 app.register_blueprint(place_controller)
 app.register_blueprint(review_controller.py)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8081, debug=True)
